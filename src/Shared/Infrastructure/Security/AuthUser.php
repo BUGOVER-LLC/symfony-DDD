d7778fc8d57dd82be2d95135906bbfe4eac6e098
BUGOVER <manukyan.artak.21@gmail.com>
2025-02-14 14:25:17 +0400

    CQERS SMAPLE WITH TESTS
    AUTHENTICATION

---
+++ src/Shared/Infrastructure/Security/AuthUser.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\Security;
+
+use App\Shared\Domain\Security\AuthUserInterface;
+use App\Shared\Domain\Security\UserGetterInterface;
+use PHPUnit\Framework\Assert;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class AuthUser implements UserGetterInterface
+{
+    public function __construct(private readonly Security $security, private readonly TranslatorInterface $translator)
+    {
+    }
+
+    #[\Override] public function getAuthUser(): AuthUserInterface
+    {
+        $user = $this->security->getUser();
+
+        Assert::assertNotNull($user, $this->translator->trans('unauthenticated'));
+        Assert::assertInstanceOf(
+            AuthUserInterface::class,
+            $user,
+            \sprintf('invalid user type %s', \get_class($user))
+        );
+
+        return $user;
+    }
+}
