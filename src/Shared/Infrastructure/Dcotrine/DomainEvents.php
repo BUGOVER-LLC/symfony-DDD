713ea91d01798caa2e3a193604aaf999b42092c1
BUGOVER <manukyan.artak.21@gmail.com>
2025-02-18 15:21:58 +0400

    adding CODE STANDART && LINTER

---
+++ src/Shared/Infrastructure/Dcotrine/DomainEvents.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\Dcotrine;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\PostPersistEventArgs;
+use Doctrine\ORM\Events;
+
+class DomainEvents implements EventSubscriber
+{
+    public function __construct()
+    {
+    }
+
+    #[\Override] public function getSubscribedEvents(): array
+    {
+        return [
+            Events::postPersist,
+            Events::postRemove,
+            Events::postUpdate,
+            Events::postFlush,
+            Events::postLoad,
+        ];
+    }
+
+    public function postPersist(PostPersistEventArgs $args)
+    {
+    }
+}
